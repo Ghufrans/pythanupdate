3a106b699f91e56b1bb03a27d42cc29b72b97132
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-06 07:53:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
 import streamlit as st
+
 st.title("Alkhidmat")
 st.title("Welcome Bano Qabil")
 st.title("Python CIT")
 st.title("PCHMC Campus")
 st.title("Korangi Campus")
-a = 7
-b = 2
-
-# addition
-print ('Sum: ', a + b)  
-
-# subtraction
-print ('Subtraction: ', a - b)   
-
-# multiplication
-print ('Multiplication: ', a * b)  
-
-# division
-print ('Division: ', a / b) 
-
-# floor division
-print ('Floor Division: ', a // b)
-
-# modulo
-print ('Modulo: ', a % b)  
-
-# a to the power b
-print ('Power: ', a ** b)   
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
+x = st.slider("Select a value")
+st.write(x, "squared is", x * x)
 
