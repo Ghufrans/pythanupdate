70ce3542441afebb87b7d3712ab66f649ffb2473
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-06 07:46:49 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,27 @@ st.title("Welcome Bano Qabil")
 st.title("Python CIT")
 st.title("PCHMC Campus")
 st.title("Korangi Campus")
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
 
