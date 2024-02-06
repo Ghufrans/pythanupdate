b7e4f5eefeba23e410ddc5d0f991b9302c73fc2b
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-06 07:40:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,5 @@ st.title("Alkhidmat")
 st.title("Welcome Bano Qabil")
 st.title("Python CIT")
 st.title("PCHMC Campus")
-import calendar  
-# Enter the month and year  
-yy = int(input("Enter year: "))  
-mm = int(input("Enter month: "))  
-  
-# display the calendar  
-print(calendar.month(yy,mm))  
-
+st.title("Korangi Campus")
 
