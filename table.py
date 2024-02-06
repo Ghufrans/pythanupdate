0cfd6d6a02a4a5b6396f1fb7802ccca59704dcae
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-06 10:54:57 -0800

    Update table.py

--- table.py
+++ table.py
@@ -4,5 +4,5 @@ st.title("Welcome Bano Qabil")
 
 #creating dataset
 
-df=pd.dataFrame({'first':[1,2,3,4,5,],'second':[6,7,8,9,10]})
+df=pd.DataFrame({'first':[1,2,3,4,5,],'second':[6,7,8,9,10]})
 st.write(df)
