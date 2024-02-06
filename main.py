081714893ae56930d81773f503464fce6a8386a3
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-06 07:54:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,5 @@ st.title("Alkhidmat")
 st.title("Welcome Bano Qabil")
 st.title("Python CIT")
 st.title("PCHMC Campus")
-st.title("Korangi Campus")
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+
 
