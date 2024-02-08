61a59dd05392bb5437a5f9095c6be7b07b965aa1
Ghufrans <157268690+Ghufrans@users.noreply.github.com>
2024-02-08 10:31:14 -0800

    Update mainkc.py

--- mainkc.py
+++ mainkc.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Welcome Bano Qabil")
-st.TITLE("PYTHON CIT TIMING 7-9 SAT,SUN")
+st.title("PYTHON CIT TIMING 7-9 SAT,SUN")
 st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
 st.title("Campus: KORANGI")
